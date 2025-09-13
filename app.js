959907e01563bc69c4abdead254d911c5f288c29
Morena Mori <moremobizz@gmail.com>
2025-09-12 23:25:58 -0300

    Agregar función para sortear un amigo de la lista y mostrar el resultado

--- app.js
+++ app.js
@@ -26,3 +26,24 @@ function limpiarCaja() {
     document.querySelector('#amigo').value = '';
     return;
 }
+
+// Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el array amigos.
+function sortearAmigo() {
+    // Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
+    if (listaAmigos.length === 0) {
+        alert('No hay amigos para sortear. Por favor, agrega al menos un amigo.');
+    } else {
+        // Seleccionar el elemento en el HTML donde se mostrará el resultado del sorteo.
+        let resultadoHTML = document.getElementById("resultado");
+
+        // Generar un índice / posición aleatoria del array con Math.random() y Math.floor(). No hace falta sumar 1 porque el array empieza en 0.
+        // Nos aseguramos que el índice esté dentro del length del array.
+        let indiceAleatorio = Math.floor(Math.random()*listaAmigos.length); 
+
+        // Mostrar el resultado del sorteo en el elemento HTML.
+        resultadoHTML.innerHTML = `Tu amigo secreto es: ${listaAmigos[indiceAleatorio]}`;
+
+        return;
+    }
+
+}
