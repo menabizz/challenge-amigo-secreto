dd4ca61dd65e1536fe5983beca8bc657757eb944
Morena Mori <moremobizz@gmail.com>
2025-09-22 04:21:36 -0300

    Implementar función para mostrar los amigos agregados en una lista HTML

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ function agregarAmigo() {
     // Limpiar el campo de entrada.
     limpiarCaja();
 
+    // Actualizar la lista de amigos en el HTML.
+    mostrarAmigo();
+
     return;
 }
 
@@ -26,6 +29,26 @@ function limpiarCaja() {
     document.querySelector('#amigo').value = '';
     return;
 }
+
+// Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML.
+function mostrarAmigo() {
+    // Obtener el elemento de la lista: Seleccionar la lista <ul> donde se mostrarán los amigos.
+    let listaHTML = document.getElementById("listaAmigos");
+
+    // Limpiar la lista existente con .innerHTML para asegurarse de que no haya duplicados al actualizar.
+    listaHTML.innerHTML = "";
+
+    // Iterar sobre el arreglo: Usa un bucle FOR para recorrer el arreglo amigos.
+    for (let iterar = 0; iterar < listaAmigos.length; iterar++) {                      
+        
+        // Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista <li> usando la iteracion.
+        let nuevoElementoLista = document.createElement("li");
+        nuevoElementoLista.textContent = listaAmigos[iterar];
+        listaHTML.appendChild(nuevoElementoLista);
+    }
+    
+    return;
+}  
 
 // Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el array amigos.
 function sortearAmigo() {
@@ -38,12 +61,12 @@ function sortearAmigo() {
 
         // Generar un índice / posición aleatoria del array con Math.random() y Math.floor(). No hace falta sumar 1 porque el array empieza en 0.
         // Nos aseguramos que el índice esté dentro del length del array.
-        let indiceAleatorio = Math.floor(Math.random()*listaAmigos.length); 
+        let numAleatorio = Math.floor(Math.random()*listaAmigos.length); 
+        console.log(numAleatorio);
 
         // Mostrar el resultado del sorteo en el elemento HTML.
-        resultadoHTML.innerHTML = `Tu amigo secreto es: ${listaAmigos[indiceAleatorio]}`;
-
-        return;
+        resultadoHTML.innerHTML = `Tu amigo secreto es: ${listaAmigos[numAleatorio]}`;
     }
 
+    return;
 }
